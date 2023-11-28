358fda698a44641060e1d1b3fe28683bf9ae4960
dlopez1591 <dlopez1591@gmail.com>
2023-11-27 21:51:27 -0500

    Admin Repository

--- src/main/java/com/onetoimprove/OneToImprove/repositories/AdminRepository.java
+++ src/main/java/com/onetoimprove/OneToImprove/repositories/AdminRepository.java
@@ -1,4 +1,9 @@
 package com.onetoimprove.OneToImprove.repositories;
 
-public interface AdminRepository {
+import com.onetoimprove.OneToImprove.models.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface AdminRepository extends JpaRepository <Admin, Long> {
 }

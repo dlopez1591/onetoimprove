041f86eab841153192eefafdbb0e045c39015eb2
dlopez1591 <dlopez1591@gmail.com>
2023-11-27 21:48:16 -0500

    creada entidad Admin

--- src/main/java/com/onetoimprove/OneToImprove/models/Admin.java
+++ src/main/java/com/onetoimprove/OneToImprove/models/Admin.java
@@ -1,4 +1,74 @@
 package com.onetoimprove.OneToImprove.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
 public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO,generator = "native")
+    @GenericGenerator(name = "native")
+    private long id;
+    private String nombre;
+    private String cargo;
+    private String email;
+    private String password;
+    private String imagen;
+
+    public Admin() {}
+
+    public Admin(String nombre, String cargo, String email, String password, String imagen) {
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.email = email;
+        this.password = password;
+        this.imagen = imagen;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
 }
